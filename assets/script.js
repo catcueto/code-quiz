51c70ef44170b77f07ab5d16a2c9b00f1093d253
catcueto <catacueto@gmail.com>
2022-07-03 16:58:57 -0400

    added form to HTML file

---
+++ assets/script.js
@@ -0,0 +1,102 @@
+alert("Hola! This took a long time, please grade nicely.");
+
+// DEFINE VARIABLES
+var secRemaining = document.querySelector("#secLeft");
+
+
+
+
+
+<><div id='question'>  Where is the correct place to insert a JavaScript file?</div><div id="answers-container">
+  <ul id='answers-list'>
+    <button class='btn' id="btn-1">The body section</button>
+    <br>
+      <button class='btn' id="btn-2">The head section</button>
+      <br>
+        <button class='btn' id="btn-3">Both the head section and the body section are correct</button>
+        <br>
+        </ul>
+        <button id="next-btn" class="next-btn btn">Next</button>
+
+      </div>
+
+      <div id="question" class="question-class">  How to write an IF statement in JavaScript?</div>
+      <div id="answers-container">
+        <ul id='answers-list'>
+          <button class='btn' id="btn-1">if x == 10 then</button>
+          <br>
+            <button class='btn' id="btn-2">if x = 10</button>
+            <br>
+              <button class='btn' id="btn-3"> if (x == 10)</button>
+              <br>
+                <button class='btn' id="btn-4">if x = 10 then</button>
+              </ul>
+              <button id="next-btn" class="next-btn btn">Next</button>
+            </div>
+
+            <div id="question" class="question-class"> How do you round the number 9.25 to the nearest
+              integer?</div>
+            <div id="answers-container">
+              <ul id='answers-list'>
+                <button class='btn' id="btn-1">round(9.25)</button>
+                <br>
+                  <button class='btn' id="btn-2"> Math.rnd(9.25)</button>
+                  <br>
+                    <button class='btn' id="btn-3"> Math.round(9.25)</button>
+                    <br>
+                      <button class='btn' id="btn-4">rnd(9.25)</button>
+                    </ul>
+                    <button id="next-btn" class="next-btn btn">Next</button>
+                  </div>
+
+                  <div id="question" class="question-class"> How can you detect the client's browser name?</div>
+                  <div id="answers-container">
+                    <ul id='answers-list'>
+                      <button class='btn' id="btn-1">navigator.appName</button>
+                      <br>
+                        <button class='btn' id="btn-2">client.navName</button>
+                        <br>
+                          <button class='btn' id="btn-3"> browser.name</button>
+                          <br>
+                          </ul>
+                          <button id="next-btn" class="next-btn btn">Next</button>
+                        </div>
+                        <div id="question" class="question-class">Which one out of the options below is a correct declaration of a JavaScript variable?</div>
+                        <div id="answers-container">
+                          <ul id='answers-list'>
+                            <button class='btn' id="btn-1"> variable dogName</button>
+                            <br>
+                              <button class='btn' id="btn-2">v dogName</button>
+                              <br>
+                                <button class='btn' id="btn-3">var-dogName</button>
+                                <br>
+                                  <button class='btn' id="btn-4">var dogName</button>
+                                  <br>
+                                  </ul>
+                                  <button id="next-btn" class="next-btn btn">Next</button>
+                                </div>
+                                <div id="question" class="question-class">Is JavaScript case-sensitive?</div>
+                                <div id="answers-container">
+                                  <ul id='answers-list'>
+                                    <button class='btn' id="btn-1">Yes</button>
+                                    <br>
+                                      <button class='btn' id="btn-2">No</button>
+                                      <br>
+                                      </ul>
+                                      <button id="next-btn" class="next-btn btn">Next</button>
+                                    </div>
+                                    <div id="question" class="question-class">What is the correct way to write a JavaScript array?</div>
+                                    <div id="answers-container">
+                                      <ul id='answers-list'>
+                                        <button class='btn' id="btn-1"> var dogBreed = 'beagle', 'boxer','boston-terrier'</button>
+                                        <br>
+                                          <button class='btn' id="btn-2">var dogBreeds = (1:'beagle',2:'boxer',3:'boston-terrier')</button>
+                                          <br>
+                                            <button class='btn' id="btn-3">var dogBreed = ['beagle', 'boxer', 'boston-terrier']</button>
+                                            <br>
+                                              <button class='btn' id="btn-3"> var dogBreed = 1 = ('beagle'), 2 = ('boxer'), 3 = ('boston-terrier')</button>
+                                              <br>
+                                              </ul>
+                                              <button id="finish-btn" class="finish-btn btn">Submit Scores</button>
+                                            </div>
+                                          </div></></></></></></></></></></></></></></></></></></></></></></>
